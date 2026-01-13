5d3d4b2b20a20ef2686d1722249a3de723236f32
Adam Gatt <adam.gatt@live.com.au>
2026-01-13 22:08:31 +1030

    Solution for Day 2 Part B

--- solutions/solution2.cpp
+++ solutions/solution2.cpp
@@ -13,25 +13,23 @@ auto Solutions::solution2() -> Answers {
     long long answerB = 0;
     
     for (const auto& range : ranges) {
-        int range_start_digits = count_digits(range.first);
-        int range_end_digits = count_digits(range.second);
 
-        for (int num_digits = range_start_digits; num_digits <= range_end_digits; ++num_digits) {
-            for (auto repetitions : potential_repetitions.at(num_digits)) {
-                int subsequence_length = num_digits / repetitions;
+        for (long long check_num = range.first; check_num <= range.second; ++check_num) {
+            std::string num_as_chars =  std::to_string(check_num);
+            int num_digits = num_as_chars.size();
 
-                long long check_num_start = calc_pow10(subsequence_length-1);
-                long long check_num_end = calc_pow10(subsequence_length);
-
-                for (long long check_num = check_num_start; check_num < check_num_end; ++check_num) {
-                    long long repeated_num = repeat(check_num, repetitions);
-                    if (within_range(repeated_num, range.first, range.second)) {
-                        answerB += repeated_num;
-                        if (repetitions == 2) {
-                            answerA += repeated_num;
-                        }
+            bool any_repetitions = false;
+            for (auto repetition_start_index : repetition_start_indices.at(num_digits)) {
+                if (num_as_chars.starts_with(num_as_chars.substr(repetition_start_index))) {
+                    any_repetitions = true;
+                    if (repetition_start_index * 2 == num_digits) {
+                        answerA += check_num;
                     }
+                    break;
                 }
+            }
+            if (any_repetitions) {
+                answerB += check_num;
             }
         }
     }
@@ -55,37 +53,4 @@ auto parse_input(const std::string& text) -> std::vector<std::pair<long long, lo
     }
     
     return ranges;
-}
-
-auto count_digits(long long number) -> int {
-    int digits = 1;
-    while (number >= 10) {
-        digits++;
-        number /= 10;
-    }
-    return digits;
-}
-
-auto calc_pow10(int exp) -> long long {
-    long long result = 1;
-    for(int i = 0; i < exp; ++i) {
-        result *= 10;
-    }
-    return result;
-}
-
-auto repeat(long long number, int repeat_count) -> long long {
-    long long result = number;
-    
-    int number_length = count_digits(number);
-    long long multiplicand = calc_pow10(number_length);
-    for (int i = 1; i < repeat_count; ++i) {
-        result = result * multiplicand + number;
-    }
-
-    return result;
-}
-
-auto within_range(long long number, long long lower_bound, long long upper_bound) -> bool {
-    return number >= lower_bound && number <= upper_bound;
 }
--- solutions/solution2.h
+++ solutions/solution2.h
@@ -7,27 +7,19 @@
 #include <utility>
 #include <vector>
 
-const std::unordered_map<int, std::unordered_set<int>> potential_repetitions {
+const std::unordered_map<int, std::unordered_set<int>> repetition_start_indices {
     {1, {}},
-    {2, {2}},
-    {3, {3}},
-    {4, {2, 4}},
-    {5, {5}},
-    {6, {2, 3, 6}},
-    {7, {7}},
-    {8, {2, 4, 8}},
-    {9, {3, 9}},
-    {10, {2, 5, 10}}
+    {2, {1}},
+    {3, {1}},
+    {4, {1, 2}},
+    {5, {1}},
+    {6, {1, 2, 3}},
+    {7, {1}},
+    {8, {1, 2, 4}},
+    {9, {1, 3}},
+    {10, {1, 2, 5}}
 };
 
 auto parse_input(const std::string& text) -> std::vector<std::pair<long long, long long>>;
-
-auto count_digits(long long number) -> int;
-
-auto calc_pow10(int exp) -> long long;
-
-auto repeat(long long number, int repeat_count) -> long long;
-
-auto within_range(long long number, long long lower_bound, long long upper_bound) -> bool;
 
 #endif
