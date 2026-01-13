2e2f3fa6d537fb77a82a2c0680b5b1062cd77ee3
Adam Gatt <adam.gatt@live.com.au>
2026-01-13 22:56:50 +1030

    Clean up

--- solutions/solution3.cpp
+++ solutions/solution3.cpp
@@ -19,12 +19,14 @@ auto Solutions::solution3() -> Answers {
 }
 
 auto max_joltage_with_n_digits(const std::string& battery, int digits) -> long long {
-    auto search_from_index = battery.begin() - 1;
+    auto search_from_index = battery.begin();
 
     std::string selected_digits(digits, '0');
     for (int digit = 0; digit < digits; ++digit) {
-        search_from_index = std::max_element(search_from_index+1, battery.end()-digits+digit+1);
+        auto last_allowed_position_for_digit = battery.end() - digits + digit + 1;
+        search_from_index = std::max_element(search_from_index, last_allowed_position_for_digit);
         selected_digits[digit] = *search_from_index;
+        search_from_index++;
     }
 
     return std::stoll(selected_digits);
